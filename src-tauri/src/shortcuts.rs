3e1913b48bf643afef65e752b104ab91dde64be7
hugeblank <hugeblank@gmail.com>
2023-09-26 14:15:34 -0700

    Comment out ctrl+w
    to uncomment when #166 is merged.

--- src-tauri/src/shortcuts.rs
+++ src-tauri/src/shortcuts.rs
@@ -6,18 +6,20 @@ pub fn ready_event_handler<R: tauri::Runtime>(
     app: &tauri::AppHandle<R>
 ) {
     let manager = app.global_shortcut_manager();
-    manager.clone().register("CmdOrCtrl+w", hide_closure(app)).unwrap();
+    //manager.clone().register("CmdOrCtrl+w", hide_closure(app)).unwrap();
     manager.clone().register("CmdOrCtrl+q", close_closure(app)).unwrap();
 }
 
+/*
 fn hide_closure<R: tauri::Runtime>(app: &tauri::AppHandle<R>) -> impl Fn() {
     let window = app.get_window("main").unwrap();
-    //let app = app.clone();
+    let app = app.clone();
     return move || {
         window.hide().unwrap();
-        //tray::toggle_window_state(window.clone(), app.tray_handle_by_id(tray::TRAY_LABEL).unwrap())
+        tray::toggle_window_state(window.clone(), app.tray_handle_by_id(tray::TRAY_LABEL).unwrap())
     }
 }
+*/
 
 fn close_closure<R: tauri::Runtime>(app: &tauri::AppHandle<R>) -> impl Fn() {
     let window = app.get_window("main").unwrap();
