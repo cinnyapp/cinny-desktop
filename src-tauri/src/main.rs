aef6cf730b600b11d2ca0a180781988e77e9d3bb
Krishan <33421343+kfiven@users.noreply.github.com>
2024-09-13 22:04:35 +1000

    add tauri-local-host plugin for sw (#345)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,13 +6,24 @@
 #[cfg(target_os = "macos")]
 mod menu;
 
+use tauri::{utils::config::AppUrl, WindowUrl};
+
 fn main() {
+    let port = 44548;
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+    context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
     let builder = tauri::Builder::default();
 
     #[cfg(target_os = "macos")]
     let builder = builder.menu(menu::menu());
 
     builder
-        .run(tauri::generate_context!())
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .run(context)
         .expect("error while building tauri application")
 }
