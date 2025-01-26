140414d8df910e4f8abb3ca39f0dc87452d48224
Ben Gray <gehzumteufel@gmail.com>
2025-01-26 11:05:50 -0800

    Upgrading to tauri v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,18 +6,30 @@
 #[cfg(target_os = "macos")]
 mod menu;
 
-use tauri::{utils::config::AppUrl, WindowUrl};
+//use tauri::{utils::config::App, WebviewUrl};
+use tauri::WebviewUrl;
 
 fn main() {
     let port = 44548;
 
     let mut context = tauri::generate_context!();
     let url = format!("http://localhost:{}", port).parse().unwrap();
-    let window_url = WindowUrl::External(url);
+    let window_url = WebviewUrl::External(url);
     // rewrite the config so the IPC is enabled on this URL
-    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
-    context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
-    let builder = tauri::Builder::default();
+    //context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+    context.config_mut().build.frontend_dist = AppUrl::Url(window_url.clone());
+    //context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
+    context.config_mut().build.dev_url = AppUrl::Url(window_url.clone());
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_os::init());
 
     #[cfg(target_os = "macos")]
     let builder = builder.menu(menu::menu());
