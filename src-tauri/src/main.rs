d7b9a349f783ca4cb88727f256e8921d1505908d
Naz <ndpm13@ch-naseem.com>
2025-11-05 10:40:11 +0100

    🛠️chore: upgrade Tauri from v1 to v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,17 +6,11 @@
 #[cfg(target_os = "macos")]
 mod menu;
 
-use tauri::{utils::config::AppUrl, WindowUrl};
-
 fn main() {
     let port = 44548;
 
-    let mut context = tauri::generate_context!();
-    let url = format!("http://localhost:{}", port).parse().unwrap();
-    let window_url = WindowUrl::External(url);
-    // rewrite the config so the IPC is enabled on this URL
-    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
-    context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
+    let context = tauri::generate_context!();
+
     let builder = tauri::Builder::default();
 
     #[cfg(target_os = "macos")]
