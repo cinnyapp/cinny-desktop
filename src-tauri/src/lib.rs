8edca44253f3ce11fdf2837ccf0b85af92a245c8
pietagorh <pietagorhh@proton.me>
2026-03-30 15:55:46 +0200

    refactor: let tauri automatically handle window creation (reverts #539)
    This commit reintroduces configuration that was deleted by #539 while
    fixing the same issue (#540).
    It also directly fixes #555 while not requiring obscure-ish code
    If a tauri building setup step ever needs to be reimplemented for some
    feature you can't configure in `tauri.conf.json`,
    consider using `WebviewWindowBuilder::from_config` (as recommended in
    Tauri docs) to use the json config as a base.
    That way most of the simple config stays easy to understand and modify.
    We could also keep the old setup function and add `"create": false` in
    the config json to tell Tauri to leave that up to us.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,8 +5,6 @@
 
 // mod menu;
 
-use tauri::{webview::WebviewWindowBuilder, WebviewUrl};
-
 pub fn run() {
     let port: u16 = 44548;
     let context = tauri::generate_context!();
@@ -20,23 +18,6 @@ pub fn run() {
     builder
         .plugin(tauri_plugin_localhost::Builder::new(port).build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
-        .setup(move |app| {
-            // Dev: use devUrl from tauri.conf.json (http://localhost:8080) to support HMR
-            #[cfg(debug_assertions)]
-            let window_url = WebviewUrl::App(Default::default());
-
-            // Release: tauri-plugin-localhost serves bundled frontend assets on this port
-            #[cfg(not(debug_assertions))]
-            let window_url = {
-                let url = format!("http://localhost:{}", port).parse().unwrap();
-                WebviewUrl::External(url)
-            };
-
-            WebviewWindowBuilder::new(app, "main".to_string(), window_url)
-                .title("Cinny")
-                .build()?;
-            Ok(())
-        })
         .run(context)
         .expect("error while building tauri application");
 }
